ac52ad858cde0af53f901689340f9d4ca325d174
Stephen Piccolo <stephen_piccolo@byu.edu>
2015-09-15 22:09:43 -0600

    no message

--- code/ParseKallistoValues.py
+++ code/ParseKallistoValues.py
@@ -10,15 +10,17 @@ if not os.path.exists("%s/Counts" % inDirPath):
 inFilePaths = glob.glob("%s/*/abundance.tsv" % inDirPath)
 
 for inFilePath in inFilePaths:
-    print "Parsing values from %s" % inFilePath
     sampleID = os.path.basename(os.path.dirname(inFilePath))
-    data = [x for x in readMatrixFromFile(inFilePath) if not x[0].startswith("target_id")]
-
-    tpmData = [[x[0], x[4]] for x in data]
-    countData = [[x[0], x[3]] for x in data]
 
     outTpmFilePath = "%s/TPM/%s" % (inDirPath, sampleID)
     outCountFilePath = "%s/Counts/%s" % (inDirPath, sampleID)
 
-    writeMatrixToFile(tpmData, outTpmFilePath)
-    writeMatrixToFile(countData, outCountFilePath)
+    if not os.path.exists(outCountFilePath):
+        print "Parsing values from %s" % inFilePath
+        data = [x for x in readMatrixFromFile(inFilePath) if not x[0].startswith("target_id")]
+
+        tpmData = [[x[0], x[4]] for x in data]
+        countData = [[x[0], x[3]] for x in data]
+
+        writeMatrixToFile(tpmData, outTpmFilePath)
+        writeMatrixToFile(countData, outCountFilePath)
--- code/ParseSalmonValues.py
+++ code/ParseSalmonValues.py
@@ -11,18 +11,20 @@ if not os.path.exists("%s/FPKM" % salmonDirPath):
 salmonFilePaths = glob.glob("%s/*/quant.sf" % salmonDirPath)
 
 for inFilePath in salmonFilePaths:
-    print "Parsing values from %s" % inFilePath
     sampleID = os.path.basename(os.path.dirname(inFilePath))
-    data = [x for x in readMatrixFromFile(inFilePath) if not x[0].startswith("#")]
-
-    tpmData = [[x[0], x[2]] for x in data]
-    fpkmData = [[x[0], x[3]] for x in data]
-    countData = [[x[0], x[4]] for x in data]
 
     outTpmFilePath = "%s/TPM/%s" % (salmonDirPath, sampleID)
     outFpkmFilePath = "%s/FPKM/%s" % (salmonDirPath, sampleID)
     outCountFilePath = "%s/Counts/%s" % (salmonDirPath, sampleID)
 
-    writeMatrixToFile(tpmData, outTpmFilePath)
-    writeMatrixToFile(fpkmData, outFpkmFilePath)
-    writeMatrixToFile(countData, outCountFilePath)
+    if not os.path.exists(outCountFilePath):
+        print "Parsing values from %s" % inFilePath
+        data = [x for x in readMatrixFromFile(inFilePath) if not x[0].startswith("#")]
+
+        tpmData = [[x[0], x[2]] for x in data]
+        fpkmData = [[x[0], x[3]] for x in data]
+        countData = [[x[0], x[4]] for x in data]
+
+        writeMatrixToFile(tpmData, outTpmFilePath)
+        writeMatrixToFile(fpkmData, outFpkmFilePath)
+        writeMatrixToFile(countData, outCountFilePath)
